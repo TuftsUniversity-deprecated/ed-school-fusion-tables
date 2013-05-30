735e0f85fd1d82e12d377a509e08b5ce9b8790f1
Mike Korcynski <mkorcy@gmail.com>
2013-05-30 15:05:40 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HerokuStaticStarterRails::Application.config.secret_token = 'c8285025bca22687d61936750993de8cf4b028d8aa86470d799998817c3dc5fc8551ca2a42bf7e1b07493f31d3f9e3c6d2aa9d758108fae772b52addac4d7426'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+HerokuStaticStarterRails::Application.config.session_store :cookie_store, :key => '_heroku_static_starter_rails_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# HerokuStaticStarterRails::Application.config.session_store :active_record_store
